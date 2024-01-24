653c98214bc73ddc6794d99965a8111ce063a77e
haybr1221 <hbranchflower@gmail.com>
2024-01-23 21:33:02 -0700

    w3 prepare

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Del Taco";
+        job1._jobTitle = "Manager";
+        job1._startYear = 1706;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._company = "Google";
+        job2._jobTitle = "CTO";
+        job2._startYear = 2020;
+        job2._endYear = 2024;
+
+        Resume res = new Resume();
+        res._name = "Name: Hayley Branchflower";
+
+        res._jobs.Add(job1);
+        res._jobs.Add(job2);
+
+        res.Display();
     }
 }
