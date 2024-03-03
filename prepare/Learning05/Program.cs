b143868f157cdf4bc3d17c89b2c9b2fef7548f96
haybr1221 <hbranchflower@gmail.com>
2024-03-02 21:29:22 -0700

    w9 prep

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape> {};
+
+        Square sq = new Square("Green", 4);
+        shapes.Add(sq);
+
+        Rectangle rec = new Rectangle("Red", 3, 4);
+        shapes.Add(rec);
+
+        Circle circ = new Circle("Blue", 6);
+        shapes.Add(circ);
+
+        foreach (Shape s in shapes) {
+            string colour = s.GetColour();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {colour} shape has an area of {area}.");
+        }
     }
 }
